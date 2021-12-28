4e57b04979362978f0baebbc349b3f9fd0a9c4a3
Giacomo002 <55291910+Giacomo002@users.noreply.github.com>
2021-12-28 15:37:45 +0100

    Base gioco js cielo

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,29 @@
 var config = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
+  this.load.image("sky", "./assets/images/sky.png");
+  this.load.image("ground", "./assets/images/platform.png");
+  this.load.image("star", "./assets/images/star.png");
+  this.load.image("bomb", "./assets/images/bomb.png");
+    this.load.spritesheet("dude", "./assets/images/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
 }
 
 function create() {
+    this.add.image(400, 300, 'sky');
 }
 
-function update() {
-}
+function update() {}
